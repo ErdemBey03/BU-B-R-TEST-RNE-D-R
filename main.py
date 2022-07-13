fa5519291ee5f4541490a49230500a3369c56384
Erdem <109048333+ErdemBey03@users.noreply.github.com>
2022-07-13 13:48:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ import os
 
 bot=Client(
   "FANTAZİ ÖRNEYİ"
-  api_id = 
+  api_id = 17586299
+  api_hash = d3438b8f8cfff705c7cd1eb0ee0053dc
+  bot_token = brz sonra
   
+@bot.on_message(filter.command("start"))
+  async def start
+await bot.send_message("bruh")  
